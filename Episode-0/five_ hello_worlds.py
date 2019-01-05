97b28e0083e26aedfe1db329a478233dbb716991
Jay Miller <kjaymiller@gmail.com>
2019-01-05 07:44:56 -0800

    Create five_ hello_worlds.py

---
+++ Episode-0/five_ hello_worlds.py
@@ -0,0 +1,19 @@
+# Example 1
+print('hello world!')
+
+# Example 2
+message = 'hello world!'
+print(message)
+
+# Example 3
+greeting = 'hello'
+name = 'world'
+print(greeting, name + '!')
+
+# Example 4
+name = 'world'
+print('hello {}!'.format(name))
+
+# Example 5
+name = 'world'
+print(f'hello {name}!
